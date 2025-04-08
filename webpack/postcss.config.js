6d6d3de9aba5087f7158905a590d65ad9cdf559d
Cameron <cameronterry@protonmail.com>
2025-04-08 18:03:56 +0100

    Added postcss mixins to the postcss config.

--- webpack/postcss.config.js
+++ webpack/postcss.config.js
@@ -1,11 +1,12 @@
 module.exports = ( { env } ) => ( {
-  plugins: {
-    'postcss-import': {},
-    'postcss-preset-env': {
-      browsers: 'last 2 versions',
-      features: {
-        'nesting-rules': true,
-      },
-    }
-  },
+	plugins: {
+		'postcss-import': {},
+		'postcss-mixins': {},
+		'postcss-preset-env': {
+		browsers: 'last 2 versions',
+			features: {
+				'nesting-rules': true,
+			},
+		}
+	},
 } );
