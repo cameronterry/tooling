5057def8afd649e2aaf683079573d6c02bb220f2
Cameron <cameronterry@protonmail.com>
2025-10-18 16:27:23 +0100

    Fixed an issue where PostCSS settings were not applied and the CSS was incorrectly processed.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -192,7 +192,21 @@ module.exports = {
 							url: false,
 						}
 					},
-					require.resolve( 'postcss-loader' ),
+					{
+						loader: require.resolve( 'postcss-loader' ),
+						options: {
+							plugins: {
+								'postcss-import': {},
+								'postcss-mixins': {},
+								'postcss-preset-env': {
+									browsers: 'last 2 versions',
+									features: {
+										'nesting-rules': true,
+									},
+								}
+							},
+						},
+					},
 				]
 			}
 		]
